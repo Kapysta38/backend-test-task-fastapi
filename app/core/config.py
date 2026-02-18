a753ddc8ce6af11a5698c9e8cef934b64c2ba9d1
Lev <vinnipyx38@gmail.com>
2026-02-18 16:54:03 +0500

    feat(db): add DB env vars

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,7 @@
 from typing import Literal
 
+from pydantic import computed_field
+from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings
 
 
@@ -12,6 +14,26 @@ class Settings(BaseSettings):
     DOCS_URL_PATH: str = "/api/docs"
     REDOC_URL_PATH: str = "/api/redoc"
     API_VERSION: str = "v1"
+
+    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "changethis"
+    POSTGRES_DB: str = "db"
+
+    @computed_field(return_type=str)
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> str:
+        return str(
+            MultiHostUrl.build(
+                scheme="postgresql+psycopg",
+                username=self.POSTGRES_USER,
+                password=self.POSTGRES_PASSWORD,
+                host=self.POSTGRES_SERVER,
+                port=self.POSTGRES_PORT,
+                path=self.POSTGRES_DB,
+            )
+        )
 
 
 settings = Settings()
